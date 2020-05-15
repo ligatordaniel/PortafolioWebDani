8525645bd2f2e5cdc8863ad2a7695d7c962eeac3
ligatorDaniel <danielulloa256@gmail.com>
2020-05-14 23:21:24 -0400

    responsive terminado12

--- assets/js/animacionesScroll.js
+++ assets/js/animacionesScroll.js
@@ -1,4 +1,4 @@
-/*
+
 function apareceScroll(){
     const html = document.getElementsByTagName('html')[0];
     const elementoAparece = document.getElementsByClassName('animadoDerecha');      
@@ -20,8 +20,6 @@ function apareceScroll(){
 
 apareceScroll(); 
 
-
-*/
 
 
 /*var intervalID = window.setInterval(miFuncion, 500);
